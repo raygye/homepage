b89e4468c63b0984851128ca76ffe683fe8189c7
yeG10 <garyye2007@gmail.com>
2020-01-07 01:08:57 -0500

    revert copying

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById("mail").onmouseover = function() {
     document.getElementById("mailText").style.opacity = "1";
-}
+};
 document.getElementById("mail").onmouseout = function() {
         document.getElementById("mailText").style.opacity = "0";
         document.getElementById("mailText").innerHTML = "Click to copy my email to your clipboard";
-}
+};
 document.getElementById("mail").onclick = function() {
     let mail = document.getElementById("invis");
     mail.select();
     document.execCommand("copy");
     document.getElementById("mailText").innerHTML = "Copied!";
-}
+};
