c143f16f2baf237471fa0bb2dd88147544f93e1f
yeG10 <garyye2007@gmail.com>
2020-01-06 23:38:19 -0500

    email is copied on click

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ document.getElementById("mail").onmouseover = function() {
 }
 document.getElementById("mail").onmouseout = function() {
         document.getElementById("mailText").style.opacity = "0";
+        document.getElementById("mailText").innerHTML = "Click to copy my email to your clipboard";
+}
+document.getElementById("mail").onclick = function() {
+    let mail = document.getElementById("invis");
+    mail.select();
+    document.execCommand("copy");
+    document.getElementById("mailText").innerHTML = "Copied!";
 }
