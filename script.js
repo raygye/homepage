e37e7d989c02b93b3c30d468f7658e4708d0908d
yeG10 <garyye2007@gmail.com>
2020-01-06 20:07:46 -0500

    1.1

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("mail").onmouseover = function() {
+    document.getElementById("mailText").style.opacity = "1";
+}
+document.getElementById("mail").onmouseout = function() {
+        document.getElementById("mailText").style.opacity = "0";
+}
